21d3112835fe02b1778fb9d728b66cffcf196c45
seble <servus@seble.info>
2016-10-28 11:10:59 +0200

    Batch name field in frontend

--- core/forms.py
+++ core/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Transaction
 
 class TransactionForm(forms.ModelForm):
+    batch_name = forms.CharField(disabled=True)
     class Meta:
         model = Transaction
         fields = [
@@ -9,6 +10,10 @@ class TransactionForm(forms.ModelForm):
             "by_user",
             "date",
             "batch",
+            "batch_name",
             "amount",
             "comment"
         ]
+        widgets = {
+            'batch': forms.TextInput(attrs={'size': 6, }),
+        }
